d2650d2c72aaf6f6c0f75478d937db12131d2b08
Ramon Medeiro <r.medeiro10@gmail.com>
2023-04-26 11:49:06 -0300

    feat: inserindo condicionais à função principal

--- HPy/main.py
+++ HPy/main.py
@@ -1,6 +1,5 @@
-from specials import Psi
+from specials import *
 import numpy as np
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -12,7 +11,6 @@ GREEN = '\033[1;32m'
 RED = '\033[1;31m'
 
 # MAIN FUNCTION
-
 
 banner = '''
 		   \u001b[36m
@@ -52,34 +50,35 @@ while(True):
 			l = int(input(BOLD + 'Value for L: '))
 			print(BOLD + 'Wait...\n')
 
-			r = np.linspace(0, 10, 100)
-			R = Psi.radial(r, n, l)
+			if n <= l or n <= 0:
+				print('valor de n <= l ou n <= 0,\nlembrando que a regra é: l = n - 1 ou n > 0')
+				break
+			else:
+				r = np.linspace(0, 10, 100)
+				R = radial(r, n, l)
 
-			fig = plt.figure(figsize=(10,10))
+				# configs
+				sns.set_style('darkgrid')
+				fig = plt.figure(figsize=(10,10))
 
-			# FIRST GRAPH
-			#fig.add_subplot(211)
+				# primeiro gráfico
+				fig.add_subplot(211)
+				plt.plot(r, R, linewidth=3, c='black')
+				plt.ylabel(f'$R_{n},_{l}(r)$', fontsize=25)
+				plt.axhline(linewidth=2, linestyle="--", color='r')
+				plt.xlim(0.0, 10.0)
 
-			sns.lineplot(r, R, linewidth=3, c='black')
-			#plt.ylabel(f'$R_{n},_{l}(r)$', fontsize=25)
-			##plt.axhline(linewidth=2, linestyle="--", color='r')
-			#plt.grid('True')
-			#plt.xlim(0.0, 10.0)
+				# segundo gráfico
+				fig.add_subplot(212)
+				plt.plot(r, (R**2)*(r**2), linewidth=3, c='black')
+				plt.xlabel('$r [a_0]$',fontsize=25)
+				plt.ylabel(f'$[R_{n},_{l}(r)]^{2}r^{2}$', fontsize=25)
+				plt.axhline(linewidth=2, linestyle="--", color='r')
+				plt.xlim(0.0, 10.0)
 
-
-			# SECOND GRAPH
-			fig.add_subplot(212)
-			plt.plot(r, (R**2)*(r**2), linewidth=3, c='black')
-			plt.xlabel('$r [a_0]$',fontsize=25)
-			plt.ylabel(f'$[R_{n},_{l}(r)]^{2}r^{2}$', fontsize=25)
-			plt.axhline(linewidth=2, linestyle="--", color='r')
-			plt.grid('True')
-			plt.xlim(0.0, 10.0)
-
-
-			# SHOW FIG
-			plt.show()
-			print()
+				# SHOW FIG
+				plt.show()
+				print()
 
 		elif choice == 2:
 			print()
@@ -88,32 +87,36 @@ while(True):
 			m = int(input(BOLD + 'Value for M: '))
 			print(BOLD + 'Wait...\n')
 
-			phi = np.linspace(0, np.pi, 100)
-			theta = np.linspace(0, 2*np.pi, 100)
-			phi, theta = np.meshgrid(phi, theta)
+			if l < 0 or np.abs(m) > l:
+				print('valor de l < 0 ou abs(m) > l,\nlembrando que a regra é: l > 0 ou abs(m) <= l')
+				break
+			else:
+				phi = np.linspace(0, np.pi, 100)
+				theta = np.linspace(0, 2*np.pi, 100)
+				phi, theta = np.meshgrid(phi, theta)
 
-			Ylm = Psi.angular(m, l, theta, phi)		
+				Ylm = angular(m, l, theta, phi)		
 
-			# SPHERICAL TO XYZ
-			R = abs(Ylm)
-			x = R * np.sin(phi) * np.cos(theta)
-			y = R * np.sin(phi) * np.sin(theta)
-			z = R * np.cos(phi)
+				# SPHERICAL TO XYZ
+				R = abs(Ylm)
+				x = R * np.sin(phi) * np.cos(theta)
+				y = R * np.sin(phi) * np.sin(theta)
+				z = R * np.cos(phi)
 
-			# Plot the surface
-			cmap = plt.get_cmap('twilight_shifted')
-			N = mcolors.Normalize(vmin=z.min(), vmax=z.max()) # NORMALIZATION OF Ylm
+				# Plot the surface
+				cmap = plt.get_cmap('twilight_shifted')
+				N = mcolors.Normalize(vmin=z.min(), vmax=z.max()) # NORMALIZATION OF Ylm
 
 
-			fig = plt.figure(figsize=(10,10))
-			ax = fig.add_subplot(111, projection='3d')
-			ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=plt.get_cmap('twilight_shifted'), facecolors=cmap(N(R)), lw = 0, antialiased=True)
-			ax.set_title(fr'$Y_{l}^{m}(\theta, \phi)$', fontsize=20)
-			ax.set_xlabel(r'$x$', fontsize=13)
-			ax.set_ylabel(r'$y$', fontsize=13)
-			ax.set_zlabel(r'$z$', fontsize=13)
+				fig = plt.figure(figsize=(10,10))
+				ax = fig.add_subplot(111, projection='3d')
+				ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=plt.get_cmap('twilight_shifted'), facecolors=cmap(N(R)), lw = 0, antialiased=True)
+				ax.set_title(fr'$Y_{l}^{m}(\theta, \phi)$', fontsize=20)
+				ax.set_xlabel(r'$x$', fontsize=13)
+				ax.set_ylabel(r'$y$', fontsize=13)
+				ax.set_zlabel(r'$z$', fontsize=13)
 
-			plt.show()
+				plt.show()
 
 
 		elif choice == 3:
